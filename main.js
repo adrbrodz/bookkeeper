afef78ee61934fe1398f5715c38d7c491da021a6
Adrian Brodziński <adrbrodz@gmail.com>
2020-12-30 13:53:14 +0100

    fetch (async/await)

--- main.js
+++ main.js
@@ -1,21 +1,38 @@
-const url = 'http://openlibrary.org/search.json?title=room+of+ones';
+const url = 'http://openlibrary.org/search.json?title=the+lord+of+the';
 
-function getData(url, callback) {
-  fetch(url)
-  .then(res => res.json())
-  .then(data => {
-    for (let book in data.docs) {
-      book_info = data.docs[book]
-      break
-    }
-  callback(book_info);
+function fetchData(url) {
+  return new Promise((resolve, reject) => {
+    console.log(`Making Request to ${url}`)
+    resolve(
+      fetch(url)
+      .then(result => result.json())
+      .then(data => {
+        for (let book in data.docs) {
+          return data.docs[book]
+      }})
+    )
+    reject('No matches found. Please try another search.')
   })
-};
-function getBookInfo() {
-  getData(url, book_info => {
-    title = book_info.title
-    author = book_info.author_name[0]
-    document.getElementById("question").innerHTML = 
-    `<h2>Did you mean <span>${title}</span> by <span>${author}?</span></h2>`
-  });
 }
+
+function processFetch(response) {
+  return new Promise((resolve, reject) => {
+    console.log('Processing response')
+    resolve(response)
+  })
+}
+
+async function doWork() {
+  const response = await fetchData(url);
+  console.log('Response received')
+  const book_info = await processFetch(response)
+  updateQuestion(book_info);
+}
+
+function updateQuestion(book_info) {
+  title = book_info.title;
+  author = book_info.author_name[0];
+  document.getElementById("question").innerHTML = 
+  `<h2>Did you mean <span>${title}</span> by <span>${author}?</span></h2>`
+}
+doWork();
