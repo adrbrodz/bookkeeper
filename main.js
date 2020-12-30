22a1c1ed840bea7c38d83068b704e42f2945351a
Adrian Brodziński <adrbrodz@gmail.com>
2020-12-30 18:28:55 +0100

    generateUrl()

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-const url = 'http://openlibrary.org/search.json?title=secret+garden';
-
 function fetchData(url) {
   return new Promise((resolve, reject) => {
     console.log(`Making Request to ${url}`)
@@ -13,27 +11,44 @@ function fetchData(url) {
     )
   })
 }
-
 function processFetch(response) {
   return new Promise((resolve, reject) => {
     resolve(response)
   })
 }
-
 function updateQuestion(book_info) {
   try {
     title = book_info.title;
     author = book_info.author_name[0];
     document.getElementById("question").innerHTML = 
-    `<h2>Did you mean <span>${title}</span> by <span>${author}?</span></h2>`
+    `<h2>Did you mean <span>${title}</span> by <span>${author}</span>?</h2>`
   } catch (err) {
     document.getElementById("question").innerHTML =
     `<h2>No matches found. Please try another search.</h2>`
   }
 }
-
 async function searchTitle() {
-  const response = await fetchData(url);
-  const book_info = await processFetch(response)
-  updateQuestion(book_info);
+  if ( document.getElementById("site-search").value != "" ) {
+    const url = generateUrl();
+    document.getElementById("question").innerHTML = `<h2>Searching...</h2>`
+    const response = await fetchData(url);
+    const book_info = await processFetch(response)
+    updateQuestion(book_info);
+  }
+}
+function generateUrl() {
+  const searchInput = getInput();
+  clearInput();
+  const url = prepareUrl();
+  return url;
+
+  function prepareUrl() {
+    return `http://openlibrary.org/search.json?title=${searchInput}`
+  }
+  function getInput() {
+    return document.getElementById("site-search").value.replace(/ /g, '+')
+  }
+  function clearInput() {
+    document.getElementById("site-search").value = ""
+  }
 }
